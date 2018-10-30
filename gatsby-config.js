99d04d00ae48d4236c376401f5c1bfee96d515f4
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-10-30 23:02:17 +0000

    Install filesystem & remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark',
   ],
 }
