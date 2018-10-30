a661b273b14dd919936ad58b6e599a9a63a8a028
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-10-30 23:03:09 +0000

    Start on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
+const query = graphql`
+  query IndexPageQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          timeToRead
+          excerpt
+        }
+      }
+    }
+  }
+`
+
 const IndexPage = () => (
   <Layout>
-    <h1>Hello world</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <StaticQuery
+      query={query}
+      render={data => (
+        <>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3>{node.frontmatter.title}</h3>
+              <span>{node.frontmatter.date}</span>
+              <span>{node.timeToRead}</span>
+              <p>{node.excerpt}</p>
+            </div>
+          ))}
+        </>
+      )}
+    />
   </Layout>
 )
 
