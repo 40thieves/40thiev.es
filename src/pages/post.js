95496a91b8e607396eeed8eabe180ebc3a855d1f
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-11-03 18:50:09 +0000

    Show content on post page

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,5 +1,43 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const Post = () => <div>POST</div>
+import Layout from '../components/layout'
+import PostHeader from '../components/post-header'
+
+export const query = graphql`
+  query PageQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      fields {
+        slug
+      }
+      html
+    }
+  }
+`
+
+const Post = ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <main>
+        <article className="max-w-md mx-auto mt-16">
+          <PostHeader
+            slug={post.fields.slug}
+            title={post.frontmatter.title}
+            date={post.frontmatter.date}
+          />
+          <section
+            className="ml-20"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </article>
+      </main>
+    </Layout>
+  )
+}
 
 export default Post
