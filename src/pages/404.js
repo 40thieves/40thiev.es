b5cfe4b5120f731da6d69a2b8d31e86861bcac75
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-08-10 16:26:00 +0100

    Style & improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,15 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <article className="max-w-3xl mx-8 sm:mx-auto mt-16">
+      <h1 className="text-4xl mb-4">Page Not Found</h1>
+      <p>
+        There's no page here{' '}
+        <span role="img" aria-label="crying emoji">
+          😢
+        </span>
+      </p>
+    </article>
   </Layout>
 )
 
