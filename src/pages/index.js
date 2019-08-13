70e51f0acea1d3536128b8c830e333461cb11074
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-08-13 23:04:40 +0100

    Refactor to useStaticQuery hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,54 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
 import PostHeader from '../components/post-header'
 
-const query = graphql`
-  query IndexPageQuery {
-    allMarkdownRemark(
-      filter: { frontmatter: { date: { ne: null } } }
-      sort: { order: DESC, fields: [frontmatter___date] }
-    ) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query IndexPageQuery {
+        allMarkdownRemark(
+          filter: { frontmatter: { date: { ne: null } } }
+          sort: { order: DESC, fields: [frontmatter___date] }
+        ) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date
+              }
+              fields {
+                slug
+              }
+              timeToRead
+              excerpt
+            }
           }
-          fields {
-            slug
-          }
-          timeToRead
-          excerpt
         }
       }
-    }
-  }
-`
+    `
+  )
 
-const IndexPage = () => (
-  <Layout>
-    <Head />
-    <StaticQuery
-      query={query}
-      render={data => (
-        <>
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <article key={node.id} className="max-w-3xl mx-8 sm:mx-auto mt-16">
-              <PostHeader
-                slug={node.fields.slug}
-                title={node.frontmatter.title}
-                date={node.frontmatter.date}
-                readTime={node.timeToRead}
-              />
-              <section className="sm:ml-24 mt-4">
-                <p>{node.excerpt}</p>
-              </section>
-            </article>
-          ))}
-        </>
-      )}
-    />
-  </Layout>
-)
-
+  return (
+    <Layout>
+      <Head />
+      {allMarkdownRemark.edges.map(({ node }) => (
+        <article key={node.id} className="max-w-3xl mx-8 sm:mx-auto mt-16">
+          <PostHeader
+            slug={node.fields.slug}
+            title={node.frontmatter.title}
+            date={node.frontmatter.date}
+            readTime={node.timeToRead}
+          />
+          <section className="sm:ml-24 mt-4">
+            <p>{node.excerpt}</p>
+          </section>
+        </article>
+      ))}
+    </Layout>
+  )
+}
 export default IndexPage
